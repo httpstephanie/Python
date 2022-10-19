06c2a8d09a6a674e65106b0457d368b2913753d3
httpstephanie <estefirbz@gmail.com>
2022-10-19 14:18:30 -0500

    Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+#mi_tuple = (1,2,(10,20),4)
+#Si se puede anidar
+#print(mi_tuple[2][0]) #Aqui te imprime el numero 10 porque es el indice 0 del anidado 2
+#ASIGNAR CONTENIDO DE TUPLES A TABLAS
+#Asi imprimen los valores de tabla 
+#t =(1,2,3)
+#x,y,z = t
+#print(x,y,z)t =(1,2,3)
+#------------------------------------------
+#Contar cantidad de apariciones de un valor
+t = (1,2,3,1)
+print(t.count(1))
